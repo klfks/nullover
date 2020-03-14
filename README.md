4b60ae95d0b35a522244e981259a604bc8fdd0ff
skoflok <skoflok@gmail.com>
2020-03-14 14:36:20 +0400

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Klfks@nullover
+
+### PutIn function
+
+Put in value and receive ZERO.
+Reset all input to nill(0)!!!
+Important FUNCTION.
+SPECIAL projeCT@!
+
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Put In any value to function and return the nill
+ * @param {*} value Any type
+ */
+function putin(value)
+{
+    return 0;
+}
+
+module.exports = {
+    putin: putin
+}
